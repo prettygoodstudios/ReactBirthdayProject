0ad3743551648fca1bd85a940e1316814c79ca41
Miguel Rust <hava12soft@gmail.com>
2018-06-25 11:23:16 -0600

    Built out initial grid

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Birthday</h1>
+      <div className="grid">
+        <div>
+          hi
+        </div>
+        <div>
+          hi
+        </div>
+        <div>
+          hi
+        </div>
+        <div className="grid__skew-light-one">
+          hi
+        </div>
+        <div className="grid__skew-light-two">
+          hi
+        </div>
+        <div className="grid__skew-light-three">
+          hi
+        </div>
       </div>
     );
   }
