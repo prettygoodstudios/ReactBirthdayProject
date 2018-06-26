cc1e93c87e12ded7dacd3e5b7aeadda15b62588a
Miguel Rust <hava12soft@gmail.com>
2018-06-26 08:40:22 -0600

    Added the date picker.

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Picker from "./picker";
 export default class App extends Component {
   render() {
     return (
       <div className="grid">
-      <h1 className="grid__title">Birthday Countdown</h1>
+        <h1 className="grid__title">Birthday Countdown</h1>
         <div className="grid__skew-dark-one">
         </div>
         <div className="grid__skew-dark-two">
@@ -17,6 +17,7 @@ export default class App extends Component {
         </div>
         <div className="grid__skew-light-three">
         </div>
+        <Picker />
       </div>
     );
   }
