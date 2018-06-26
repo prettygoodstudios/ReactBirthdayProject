cbdeaac9c49e69cfa46e82db3c589ac0ebf053c6
Miguel Rust <hava12soft@gmail.com>
2018-06-26 09:29:35 -0600

    Created button component.

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Picker from "./picker";
+import Button from "./button";
 export default class App extends Component {
   render() {
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
+        <Button title="Generate Countdown"/>
         <div className="grid__skew-dark-one">
         </div>
         <div className="grid__skew-dark-two">
---
+++ src/components/button.js
@@ -0,0 +1,7 @@
+import React, {Component} from "react";
+const Button = (props) => {
+  return(
+    <button className="button">{props.title}</button>
+  );
+}
+export default Button;
